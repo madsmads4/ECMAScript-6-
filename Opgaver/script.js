5309ab90b999d6c33ca8d78d43288d94b25e87ee
madsmads4 <madsolsen213@gmail.com>
2025-09-22 07:39:34 +0200

    New update

--- Opgaver/script.js
+++ Opgaver/script.js
@@ -3,9 +3,11 @@
 // function generateRandomNumber(minNum = 66, maxNum = 500) {
 //     return Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
 // }
-
 // const randomNumber = generateRandomNumber();
 // console.log(`Execute Order ${randomNumber}`);
+
+
+
 
 
 // // Valuta omregner
@@ -18,9 +20,10 @@
 //     }).format(dkkAmount)
 //     return dkkAmount.toFixed(2);
 // }
-
 // const beløbIDKK = convertCurrency(150);
 // console.log(`${beløbIDKK}`);
+
+
 
 
 // //Fremtidig dato
@@ -31,24 +34,25 @@
 // }
 // console.log(findFutureDate(5))
 
+
+
+
+
 //Array med helte
-const canLiftMjolnir = [
-  "Thor", "IronMan", "CaptainAmerica", "Hulk", "BlackWidow", 
-  "SpiderMan", "DoctorStrange", "BlackPanther", "ScarletWitch", 
-  "Vision", "Falcon", "WarMachine"
-];
+// const canLiftMjolnir = [
+//   "Thor", "IronMan", "CaptainAmerica", "Hulk", "BlackWidow", 
+//   "SpiderMan", "DoctorStrange", "BlackPanther", "ScarletWitch", 
+//   "Vision", "Falcon", "WarMachine"
+// ];
 
-//Min men med arrow functions
-const parseNumbers = heroInput => {
-  const getHero = heroInput.map(hero => parseInt(hero));
-  return `${getHero.join('')} Batman`;
-};
+// //Min men med arrow functions
+// const parseNumbers = heroInput => {
+//   const getHero = heroInput.slice(0, 12).map(hero => parseInt(hero));
+//   return `${getHero.join('')} Batman`;
+// };
+// console.log(parseNumbers(canLiftMjolnir));
 
-console.log(parseNumbers(canLiftMjolnir));
-
-
-
-// Marc helte funktion
+// Marc Batman sang
 // function parseNumbers(heroInput) {
 //     let getHero = heroInput.map(function(hero) {
 //         return parseInt(hero);
@@ -56,5 +60,31 @@ console.log(parseNumbers(canLiftMjolnir));
 //     return `${getHero.join('')} Batman`;
 // }
 // console.log(parseNumbers(canLiftMjolnir));
+
+
+//Liste af superhelte som bliver vist på siden
+// const superHeroes = [
+//         { heroname: "Thor", age: 1000, powers: ["Strength", "Flight"] },
+//         { heroname: "Doctor Strange", age: 35, powers: ["Flight", "Teleportation"] },
+//         { heroname: "Spider Man", age: 17, powers: ["Agility", "Web Shooting"] },
+//         { heroname: "Iron Man", age: 48, powers: ["Technology", "Armor"] },
+//         { heroname: "Black Panther", age: 35, powers: ["Strength", "Stealth"] },
+//         { heroname: "Captain Marvel", age: 34, powers: ["Energy", "Flight"] },
+//         { heroname: "Scarlet Witch", age: 29, powers: ["Chaos", "Reality"] },
+//         { heroname: "Hulk", age: 40, powers: ["Strength", "Regeneration"] },
+//         { heroname: "Ant-Man", age: 38, powers: ["Size-Shifting", "Strength"] },
+//         { heroname: "Wolverine", age: 150, powers: ["Regeneration", "Adamantium Claws"] }
+//     ];
+
+//     const heroList = document.getElementById('heroList');
+
+//     heroList.innerHTML = superHeroes.map(hero => `
+//         <li>
+//             <div class="hero-name">${hero.heroname}</div>
+//             <div class="hero-age">Age: ${hero.age}</div>
+//             <div class="hero-powers">Powers: ${hero.powers.join(', ')}</div>
+//         </li>
+//     `).join('');
+
 
 
